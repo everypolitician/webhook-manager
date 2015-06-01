eb706220076ae264c21de10f1afc6bce1be4432d
Chris Mytton <chrismytton@gmail.com>
2015-06-01 10:16:08 +0100

    Add worker to update viewer-sinatra
    When new data is detected in everypolitician-data we want to update
    viewer-sinatra with that data by adding it to the src/{country}.src
    file. This is done on push events by checking for newly added files
    which match final.json.

--- app.rb
+++ app.rb
@@ -10,6 +10,27 @@ module Github
   def github
     @github ||= Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
   end
+
+  def clone_url(uri)
+    repo_clone_url = URI.parse(uri)
+    repo_clone_url.user = github.login
+    repo_clone_url.password = github.access_token
+    repo_clone_url
+  end
+
+  def with_tmp_dir(&block)
+    Dir.mktmpdir do |tmp_dir|
+      Dir.chdir(tmp_dir, &block)
+    end
+  end
+
+  def git_clone(repo_name)
+    repo = github.repository(repo_name)
+    with_tmp_dir do
+      `git clone -q #{clone_url(repo.clone_url)} .`
+      yield
+    end
+  end
 end
 
 # Background job that merges pull requests.
@@ -22,12 +43,53 @@ class MergeJob
   end
 end
 
+class UpdateViewerSinatraJob
+  include Sidekiq::Worker
+  include Github
+
+  def perform(push)
+    added_files = push['commits'].map do |commit|
+      commit['added']
+    end
+    added_files = added_files.flatten.uniq
+    final_json = added_files.select { |file| file =~ /final.json$/ }
+    files_to_update = {}
+    git_clone(push['repository']['full_name']) do |dir|
+      final_json.each do |file|
+        match = file.match(/^data\/(?<country>\w+)/)
+        country = match[:country]
+        sha_updated = `git log --format='%h|%at' -1 #{file}`
+        files_to_update[country] = sha_updated
+      end
+    end
+
+    git_clone('chrismytton/viewer-sinatra') do
+      git_config = "-c user.name='#{github.login}' -c user.email='#{github.emails.first[:email]}'"
+      files_to_update.each do |country, sha_updated|
+        path = "src/#{country}.src"
+        File.open(path, 'w') { |file| file.puts(sha_updated) }
+        `git add .`
+        message = "#{country}: Initial data"
+        `git #{git_config} commit --message="#{message}"`
+      end
+      `git push --quiet origin master`
+    end
+  end
+end
+
 configure do
   set :github_webhook_secret, ENV['GITHUB_WEBHOOK_SECRET']
 end
 
 helpers do
   HMAC_DIGEST = OpenSSL::Digest.new('sha1')
+
+  def payload_body
+    request.body.rewind
+    payload_body = request.body.read
+    verify_signature(payload_body)
+    payload_body
+  end
 
   # Taken from https://developer.github.com/webhooks/securing/
   def verify_signature(payload_body)
@@ -45,9 +107,6 @@ helpers do
 end
 
 post '/github_events' do
-  request.body.rewind
-  payload_body = request.body.read
-  verify_signature(payload_body)
   pull_request = JSON.parse(payload_body)
   type = request.env['HTTP_X_GITHUB_EVENT']
   action = pull_request['action']
@@ -57,6 +116,16 @@ post '/github_events' do
     repo = pull_request['repository']['full_name']
     number = pull_request['number']
     MergeJob.perform_async(repo, number)
+  end
+  'OK'
+end
+
+# Check for new countries
+post '/everypolitician-data-push' do
+  type = request.env['HTTP_X_GITHUB_EVENT']
+  if type == 'push'
+    push = JSON.parse(payload_body)
+    UpdateViewerSinatraJob.perform_async(push)
   end
   'OK'
 end
