91ec7bbbb47082bf182eef80507bbf1f3b564a47
Chris Mytton <chrismytton@gmail.com>
2015-06-01 11:28:36 +0100

    Add everypoliticianbot as an auto-merge user
    When pull requests are received from everypoliticianbot they will be
    automatically merged.

--- app/jobs/merge_job.rb
+++ app/jobs/merge_job.rb
@@ -8,9 +8,13 @@ class MergeJob
   def perform(pull_request)
     action = pull_request['action']
     user = pull_request['pull_request']['user']['login']
-    return if action != 'opened' || user != 'seepoliticianstweetbot'
+    return if action != 'opened' || !auto_merge_users.include?(user)
     repo = pull_request['repository']['full_name']
     number = pull_request['number']
     github.merge_pull_request(repo, number)
+  end
+
+  def auto_merge_users
+    %w(everypoliticianbot seepoliticianstweetbot)
   end
 end
