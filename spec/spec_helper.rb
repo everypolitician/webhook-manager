cec0d6c5d8915d7c44e547b3a9095a8138b44e99
Chris Mytton <chrism@mysociety.org>
2015-06-05 21:36:29 +0100

    Add OmniAuth mock auth hash

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,15 @@ class Minitest::Spec
 end
 
 OmniAuth.config.test_mode = true
+
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+  provider: 'github',
+  uid: '123545',
+  info: {
+    name: 'Bob Test',
+    email: 'bob@example.org'
+  },
+  credentials: {
+    token: 'abc123'
+  }
+)
