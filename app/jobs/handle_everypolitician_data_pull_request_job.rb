5a56aefe06276f2be261f7556a90a493ea75042b
Chris Mytton <chrism@mysociety.org>
2015-06-19 15:15:31 +0100

    Simplify logic for handling PR and creating deployment
    This now correctly checks for newly opened or synchronized pull requests
    and then creates a deployment event to update viewer-sinatra.

--- app/jobs/handle_everypolitician_data_pull_request_job.rb
+++ app/jobs/handle_everypolitician_data_pull_request_job.rb
@@ -14,34 +14,21 @@ class HandleEverypoliticianDataPullRequestJob
   private
 
   def valid?
-    if %w(action synchronize).include?(pull_request['action'])
-      repository = pull_request['repository']['full_name']
-      repository == ENV['EVERYPOLITICIAN_DATA_REPO']
-    end
-    false
+    %w(opened synchronize).include?(pull_request['action']) &&
+      pull_request['repository']['full_name'] == everypolitician_data_repo
   end
 
   def create_deployment_event
     github.create_deployment(
       everypolitician_data_repo,
-      pull_request_ref,
-      payload: { pull_request_number: pull['number'] }
+      pull_request['pull_request']['head']['ref'],
+      payload: { pull_request_number: pull_request['number'] }
     )
-  end
-
-  def pull_request_ref
-    pull_request['pull_request']['head']['ref']
   end
 
   def pull_request_updated_countries_json?
-    commits = github.pull_commits(
-      everypolitician_data_repo,
-      pull_request['number']
-    )
-    files = commits.map do |commit|
-      commit['added'] + commit['modified']
-    end
-    files.flatten.uniq.include?('countries.json')
+    files = github.pull_files(everypolitician_data_repo, pull_request['number'])
+    files.map { |f| f[:filename] }.flatten.uniq.include?('countries.json')
   end
 
   def everypolitician_data_repo
