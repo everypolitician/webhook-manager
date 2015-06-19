2691ccc58bf917c92ea1b6e0977b012ef8314b97
Chris Mytton <chrism@mysociety.org>
2015-06-19 15:41:58 +0100

    Remove unused UpdateViewerSinatraJob class
    This is no longer used for updating, we now do it just with mirrored
    pull requests and deployments.

--- app/jobs.rb
+++ app/jobs.rb
@@ -1,5 +1,4 @@
 require 'app/jobs/merge_job'
-require 'app/jobs/update_viewer_sinatra_job'
 require 'app/jobs/accept_submission_job'
 require 'app/jobs/deploy_viewer_sinatra_pull_request_job'
 require 'app/jobs/handle_everypolitician_data_pull_request_job'
