68b52b40a652b0b2548d85975ae1e3b617ee1ac4
Chris Mytton <chrism@mysociety.org>
2015-07-09 16:01:07 +0100

    Send countries.json as body of the webhook

--- app/jobs/send_webhook_job.rb
+++ app/jobs/send_webhook_job.rb
@@ -3,6 +3,13 @@ class SendWebhookJob
   include Sidekiq::Worker
 
   def perform(webhook_url)
-    Faraday.post(webhook_url)
+    Faraday.post(webhook_url) do |req|
+      req.body = countries_json
+      req.headers['Content-Type'] = 'application/json'
+    end
+  end
+
+  def countries_json
+    open('https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/countries.json').read
   end
 end
