053a28b10992c96a17531be6ab785a85df681041
Chris Mytton <chrismytton@gmail.com>
2015-08-20 18:35:55 +0100

    Generate an app_id when the app is created

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,7 +8,19 @@ class Application < Sequel::Model
   end
 
   def before_create
-    self.secret ||= SecureRandom.hex(20)
+    generate_app_id
+    generate_app_secret
     super
+  end
+
+  def generate_app_id
+    loop do
+      self.app_id = SecureRandom.hex(10)
+      break unless Application.where(app_id: app_id).any?
+    end
+  end
+
+  def generate_app_secret
+    self.secret = SecureRandom.hex(20)
   end
 end
