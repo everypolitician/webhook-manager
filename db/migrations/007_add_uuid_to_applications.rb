27a077a8720ef661abbb93e877d66e379e186a4d
Chris Mytton <chrismytton@gmail.com>
2015-08-20 18:35:45 +0100

    Add app id to applications table

---
+++ db/migrations/007_add_uuid_to_applications.rb
@@ -0,0 +1,24 @@
+Sequel.migration do
+  up do
+    alter_table(:applications) do
+      add_column :app_id, String, unique: true
+    end
+    applications = from(:applications)
+    applications.all do |application|
+      loop do
+        app_id = SecureRandom.hex(10)
+        break unless Application.where(app_id: app_id).any?
+      end
+      applications.where(id: application[:id]).update(app_id: app_id)
+    end
+    alter_table(:applications) do
+      set_column_not_null :app_id
+    end
+  end
+
+  down do
+    alter_table(:applications) do
+      drop_column :app_id
+    end
+  end
+end
