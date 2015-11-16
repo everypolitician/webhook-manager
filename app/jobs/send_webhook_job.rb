f08ff10e07929a8cc529b4639ac2eeddb80cef9d
Chris Mytton <chrism@mysociety.org>
2015-11-16 11:05:33 +0000

    Don't retry webhook delivery for all countries
    If a webhook delivery fails then we don't want to keep retrying it for
    ~21 days as it can block up the queue and when delivery does succeed
    there's a good chance the data will be out of date by that time.

--- app/jobs/send_webhook_job.rb
+++ app/jobs/send_webhook_job.rb
@@ -2,6 +2,14 @@
 class SendWebhookJob
   include Sidekiq::Worker
 
+  # Discard the job immediately if it fails.
+  #
+  # FIXME: This is OK for the webhook for all countries because that usually
+  # gets triggered multiple times per day. When we add webhooks for individual
+  # countries we should then consider adding some kind of retry mechanism as
+  # these won't be delivered as frequently.
+  sidekiq_options retry: false
+
   def perform(webhook_url)
     Faraday.post(webhook_url) do |req|
       req.body = countries_json
